f109a6d8cd9027df40d0191dcf91bd3c462a1756
Balu <balubittu007@gmail.com>
2024-07-31 22:39:41 +0530

    Test APIs

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = (req, res)=> {
+    //db Operations
+};
+
+export const login = (req, res)=> {
+    //db Operations
+};
+
+export const logout = (req, res)=> {
+    //db Operations
+};
