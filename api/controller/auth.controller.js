66220f3e8e0146b118d0e7d7c26f282b635f1a64
Balu <balubittu007@gmail.com>
2024-08-05 21:38:51 +0530

    prisma user modal and register route set up

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,14 +1,27 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res)=> {
     const { username, email, password } = req.body;
     
-    //Hash the password by using bcrypt
-    const hashedPassword = await bcrypt.hash(password, 10)
-    console.log(hashedPassword);
-    //Create new user in DB
-
-
+    try{
+        //Hash the password by using bcrypt
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log(hashedPassword);
+        //Create new user in DB
+        const newuser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            },
+        });
+        console.log(newuser);
+        res.status(201).json({message: "User created successfully"});
+    }catch(err) {
+        console.log(err);
+        res.status(500).send("Failed to create user");
+    }
 };
 
 export const login = (req, res)=> {
