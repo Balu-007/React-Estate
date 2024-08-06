4995dd2e4c396f4447a038829aa72348cb780a9e
Balu <balubittu007@gmail.com>
2024-08-06 21:52:17 +0530

    Login Route - cookieParser, JWT. Logout Route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res)=> {
@@ -17,17 +18,56 @@ export const register = async (req, res)=> {
             },
         });
         console.log(newuser);
-        res.status(201).json({message: "User created successfully"});
+        res.status(201).json({"message": "User created successfully"});
     }catch(err) {
         console.log(err);
         res.status(500).send("Failed to create user");
     }
 };
 
-export const login = (req, res)=> {
+export const login = async (req, res)=> {
+    const {username, password} = req.body;
     //db Operations
+    try{
+    //Check if the user exists
+        const user = await prisma.user.findUnique({
+            where: {username},
+        });
+        if(!user) return res.status(401).send({message: "Invalid credentials"});
+
+    //Check if the password is correct
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    //Generate cookie token and send to the user
+        // res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+        const age = 1000 * 60 * 60 * 24 * 7;
+        const token = jwt.sign(
+            {
+              id: user.id,
+            //   isAdmin: false,
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+          );
+
+        res.cookie("token", token, {
+                httpOnly: true,
+                // secure:true, should be true for production
+                maxAge: age, // Sets the cookie expiry to 1 week
+            })
+            .status(200)
+            .json({message: "Login successful"});
+
+
+    }catch(err) {
+        console.log(err);
+        res.status(500).send("Failed to create user");
+    }
 };
 
 export const logout = (req, res)=> {
     //db Operations
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
