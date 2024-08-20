d41a5cda25b1cc3dffd0de11ac1dbd416fbffcda
Balu <balubittu007@gmail.com>
2024-08-20 15:26:29 +0530

    Express Js Middleware

--- api/controller/test.controller.js
+++ api/controller/test.controller.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const shoulBeLoggedIn = async (req, res) => {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+    // const token = req.cookies.token;
+    // if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-        if (err) return res.status(403).json({ message: "!Invalid Token" })
-    })
+    // jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    //     if (err) return res.status(403).json({ message: "!Invalid Token" })
+    // })
+    console.log(req.userId);
     res.status(200).json({ message: "You are Authenticated" });
 }
 
--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { shoulBeLoggedIn, shouldbeAdmin } from "../controller/test.controller.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/should-be-loggedin', shoulBeLoggedIn);
+router.get('/should-be-loggedin', verifyToken, shoulBeLoggedIn);
 router.get('/should-be-admin', shouldbeAdmin);
 
 export default router;
