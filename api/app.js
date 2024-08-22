ba27d1c579f4c4d41e2e1436d5f1bfad80b1be3d
Balu <balubittu007@gmail.com>
2024-08-22 19:21:06 +0530

    CRUD using prisma- user update, delete, get users. Integrating update user functionality with UI

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
@@ -13,6 +14,7 @@ app.use(cookieParser());
 //     res.send("it works!")
 // });
 app.use("/api/posts", postRoute);
+app.use('/api/users', userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
 
