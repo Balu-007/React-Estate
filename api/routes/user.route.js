f5899c120b8d968ed983a38236a29f9ef17e91aa
Balu <balubittu007@gmail.com>
2024-08-26 12:58:44 +0530

    Fetch profile posts

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser, savePost } from "../controller/user.controller.js";
+import { deleteUser, getUser, getUsers, updateUser, savePost, profilePosts } from "../controller/user.controller.js";
 import verifyToken from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/',  getUsers);
-router.get('/:id', verifyToken, getUser);
+// router.get('/:id', verifyToken, getUser); we are not fetching single user and to avoid colliding with profiePosts
 router.put('/:id', verifyToken, updateUser);
 router.delete('/:id', verifyToken, deleteUser);
 router.post('/save', verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
 
 export default router;
