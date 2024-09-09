f3f86230fcbf26f5d66591dd0ddc7df9660da57c
Balu <balubittu007@gmail.com>
2024-09-09 17:59:38 +0530

    Deleted old deployment urls

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,8 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    // setSocket(io("http://localhost:4000"));
-    setSocket(io("https://react-estate-socket.onrender.com"));
+    setSocket(io("http://localhost:4000"));
   }, []);
 
   useEffect(() => {
